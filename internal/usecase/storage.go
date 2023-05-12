174dac43fc95d4cd68056dc21a8fec9f892606a6
gingersamurai <nazimmalyshev47@gmail.com>
2023-05-12 13:43:31 +0300

    remade storage interface

--- internal/usecase/storage.go
+++ internal/usecase/storage.go
@@ -3,6 +3,6 @@ package usecase
 import "go-shortener/internal/entity"
 
 type Storage interface {
-	AddLink(source, mapping string) (string, error)
-	GetLink(source string) entity.Link
+	AddLink(link entity.Link) error
+	GetLink(source string) (entity.Link, error)
 }
