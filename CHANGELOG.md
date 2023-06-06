f442116ae1a8cc58fd0ea449c712913ec8ef98b2
gingersamurai <nazimmalyshev47@gmail.com>
2023-06-06 15:01:21 +0300

    created release

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+## 1.0.0
+* Service versioning swithced to `Semantic Versioning 2.0.0`
---
+++ internal/meta/version.go
@@ -0,0 +1,9 @@
+package meta
+
+const (
+	VersionMajor = "1"
+	VersionMinor = "0"
+	VersionPatch = "0"
+)
+
+const Version = VersionMajor + "." + VersionMinor + "." + VersionPatch
